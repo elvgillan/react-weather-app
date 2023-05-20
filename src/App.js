a62ceacca85549a30e22ddbba493f4baf3ad5df8
elvgillan <98256076+elvgillan@users.noreply.github.com>
2023-05-20 16:42:51 +1000

    Added footer with GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        The project was coded by{" "}
+        <a href="https://www.linkedin.com/in/eringillan/" target="_blank">
+          Erin Gillan
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/elvgillan/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
