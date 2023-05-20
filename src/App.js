73b6b75a70d48627be0492f3354c53b9860e9ab3
elvgillan <98256076+elvgillan@users.noreply.github.com>
2023-05-20 16:54:01 +1000

    Updated URLs in Footer

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         The project was coded by{" "}
-        <a href="https://www.linkedin.com/in/eringillan/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/eringillan/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Erin Gillan
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/elvgillan/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
