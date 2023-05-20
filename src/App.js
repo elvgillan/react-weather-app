cc042147cdb73fa656465e6d199c66aea4f3fa33
elvgillan <98256076+elvgillan@users.noreply.github.com>
2023-05-20 17:11:36 +1000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        The project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/eringillan/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Erin Gillan
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/elvgillan/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          The project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/eringillan/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Erin Gillan
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/elvgillan/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
